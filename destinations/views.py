2cb3565cdfbb8afad319b409b6df2479361d4637
Ei Sin <einge174@gmail.com>
2024-09-19 21:24:51 +0000

    add code for the layout on the index page and add some stylesheet

--- destinations/views.py
+++ destinations/views.py
@@ -9,7 +9,7 @@ class DestinationsList(generic.ListView):
     # model = Destination
     queryset = Destination.objects.all()
     template_name = "destinations/index.html"
-    paginate_by = 6
+    paginate_by = 3
 
     
     def dest_list(request, slug):
