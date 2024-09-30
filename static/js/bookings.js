c4e01cf55b68fe472498295353034e2c414b479a
Ei Sin <einge174@gmail.com>
2024-09-30 21:45:39 +0000

    Bug fixed for the booking.js for Delete button

--- static/js/bookings.js
+++ static/js/bookings.js
@@ -1,6 +1,5 @@
 const editButtons = document.getElementsByClassName("btn-booking-edit");
 const deleteButtons  = document.getElementsByClassName("btn-booking-delete");
-const editBookingModal = new bootstrap.Modal(document.getElementById("editBookingModal"));
 const deleteBookingModal = new bootstrap.Modal(document.getElementById("deleteBookingModal"))
 const submitButton = document.getElementById("submitButton");
 const editConfirm = document.getElementById("editConfirm");
@@ -10,9 +9,8 @@ for (let button of editButtons) {
     button.addEventListener("click", (e) => {
       let bookingId = e.target.getAttribute("data-booking_id");
       editConfirm.href = `edit_booking/${bookingId}`
-      //let bookingMonth = e.target.getAttribut("");
       //booking.booking_month = 
-      editBookingModal.show();
+      //editBookingModal.show();
     });
   }
 
