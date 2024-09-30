cd9e4ff0aceb8b0c958ce7b32c4b412ea3757e94
Ei Sin <einge174@gmail.com>
2024-09-30 22:01:56 +0000

    Bug fixed for the booking.js for Delete button

--- travelEra/settings.py
+++ travelEra/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-eshwes01-travel-vgtm7t5rik5.ws.codeinstitute-ide.net','.herokuapp.com']
 
