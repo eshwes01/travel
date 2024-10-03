fecc786a78bb74b88fffcc6c94ea7b8c630851d3
Ei Sin <einge174@gmail.com>
2024-10-03 16:25:44 +0000

    Add CSS code to fix for the Package_Detail display on mobile screen

--- static/js/bookings.js
+++ static/js/bookings.js
@@ -2,6 +2,15 @@ const deleteButtons  = document.getElementsByClassName("btn-booking-delete");
 const deleteBookingModal = new bootstrap.Modal(document.getElementById("deleteBookingModal"));
 const deleteConfirm = document.getElementById("deleteConfirm");
 
+/* Initializes deletion functionality for the provided delete buttons.
+* 
+* For each button in the `deleteButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Updates the `deleteConfirm` link's href to point to the 
+* deletion endpoint for the specific comment.
+* - Displays a confirmation modal (`deleteModal`) to prompt 
+* the user for confirmation before deletion.
+*/
   for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
       let bookingId = e.target.getAttribute("data-booking_id");
